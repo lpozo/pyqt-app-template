e00108ad3fbfd75646e28cd45b481580ee7b42ed
Leodanis Pozo Ramos <lpozor78@gmail.com>
2020-12-06 12:15:04 -0500

    Add a sample Model class

--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/model/model.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/model/model.py
@@ -1,1 +1,11 @@
-"""Model."""
+"""This module provides {{ cookiecutter.app_name }} Model class."""
+
+
+class Model:
+    # Add model code here...
+
+    def data(self):
+        pass
+
+    def setData(self):
+        pass
